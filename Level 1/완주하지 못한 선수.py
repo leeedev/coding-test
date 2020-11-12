d6a9f1d105812d0b121b3339fb08af47dea29745
leeedev <leeesangheee@gmail.com>
2020-11-12 20:45:53 +0900

    Create 완주하지 못한 선수.py

---
+++ Level 1/완주하지 못한 선수.py
@@ -0,0 +1,17 @@
+participant = ["leo", "kiki", "eden"]
+completion = ["eden", "kiki"]
+
+
+def solution(participant, completion):
+    participant.sort()
+    completion.sort()
+
+    for i in range(len(completion)):
+        if participant[i] != completion[i]:
+            return participant[i]
+            break
+    else:
+        return participant[i + 1]
+
+
+print(solution(participant, completion))
