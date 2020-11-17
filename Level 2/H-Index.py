b58561d7e84b626b40792da81c9461fb6b72c6cc
leeedev <leeesangheee@gmail.com>
2020-11-17 22:05:00 +0900

    Update H-Index.py

--- Level 2/H-Index.py
+++ Level 2/H-Index.py
@@ -3,9 +3,9 @@ citations = [3, 0, 6, 1, 5]
 
 def solution(citations):
     n = len(citations)
-    for i in range(n, -1, -1):
-        if i <= len(list(filter(lambda x: x >= i, citations))):
-            return i
+    for h in range(n, -1, -1):
+        if h <= len(list(filter(lambda x: x >= h, citations))):
+            return h
 
 
 print(solution(citations))
