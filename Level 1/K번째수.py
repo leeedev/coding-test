5f80ee8d63ec10c14dc00412f62a1cc45dafa84d
leeedev <leeesangheee@gmail.com>
2020-11-18 21:41:11 +0900

    Update K번째수.py

--- Level 1/K번째수.py
+++ Level 1/K번째수.py
@@ -4,8 +4,7 @@ commands = [[2, 5, 3], [4, 4, 1], [1, 7, 3]]
 
 def solution(array, commands):
     answer = []
-    for command in commands:
-        i, j, k = command
+    for i, j, k in commands:
         answer.append(sorted(array[i - 1:j])[k - 1])
     return answer
 
