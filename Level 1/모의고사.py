4d580cea880dfab86cc76bf03e2a09fc02b098ca
leeedev <leeesangheee@gmail.com>
2020-11-20 16:08:23 +0900

    Update 모의고사.py

--- Level 1/모의고사.py
+++ Level 1/모의고사.py
@@ -4,16 +4,16 @@ answers = [1, 3, 2, 4, 2]
 def solution(answers):
     pattern = [[1, 2, 3, 4, 5], [2, 1, 2, 3, 2, 4, 2, 5],
                [3, 3, 1, 1, 2, 2, 4, 4, 5, 5]]
-    answer = []
+    scores = []
     result = []
 
     for i in range(3):
         score = list(
             filter(lambda x: pattern[i][x[0] % len(pattern[i])] == x[1], list(enumerate(answers))))
-        answer.append(len(score))
+        scores.append(len(score))
 
-    for idx, score in enumerate(answer):
-        if score == max(answer):
+    for idx, score in enumerate(scores):
+        if score == max(scores):
             result.append(idx + 1)
 
     return result
