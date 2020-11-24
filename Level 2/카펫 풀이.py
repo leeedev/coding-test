bfb4de289ef2b19aceed955ed01ec0da89042f9b
leeedev <leeesangheee@gmail.com>
2020-11-25 00:01:18 +0900

    Create 카펫 풀이.py

---
+++ Level 2/카펫 풀이.py
@@ -0,0 +1,26 @@
+brown = 10
+yellow = 2
+
+
+def solution(brown, yellow):
+    multiply = brown + yellow
+    divisor = []
+
+    # multiply의 약수 w, h(yellow가 1 이상이므로 brown은 3 이상)
+    # 약수는 제곱수 이하까지만 돌리면 된다.
+    for i in range(3, int(multiply ** 0.5 + 1)):
+        if multiply % i == 0:
+            divisor.append(i)
+
+    if len(divisor) == 1:
+        return [int(multiply / divisor[0]), divisor[0]]
+
+    # w + h = brown / 2 + 2를 만족하는 w, h
+    for i in range(0, len(divisor)):
+        h = divisor[i]
+        w = int(multiply / h)
+        if w + h == brown / 2 + 2:
+            return [w, h]
+
+
+print(solution(brown, yellow))
